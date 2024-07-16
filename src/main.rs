dd0f40b109379e28707f2d5320983cefe29cc210
GnomedDev <david2005thomas@gmail.com>
2024-07-16 17:47:38 +0100

    Minimize crash

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,23 @@
 #![no_std]
 #![no_main]
-#![warn(rust_2018_idioms, clippy::pedantic)]
+#![warn(rust_2018_idioms)]
 #![feature(type_alias_impl_trait, concat_bytes)]
 
-use alloc::format;
-use core::{cell::RefCell, str::FromStr as _};
-use embassy_time::Timer;
-use embedded_hal_bus::spi::RefCellDevice;
-
 use embassy_executor::Spawner;
-use epd_waveshare::{
-    epd2in13_v2::Display2in13 as EpdBuffer, graphics::DisplayRotation,
-    prelude::WaveshareDisplay as _,
-};
+use embassy_net::tcp::client::TcpClientState;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    delay::Delay,
-    gpio::{self},
-    peripherals::{Peripherals, LPWR, SPI2},
+    peripherals::{Peripherals, LPWR, RSA},
     prelude::*,
     rtc_cntl::Rtc,
-    spi::{FullDuplexMode, SpiMode},
     system::SystemControl,
     timer::{timg::TimerGroup, ErasedTimer, OneShotTimer},
 };
-use rusttype::Font;
+use net::{DnsSocket, HttpClient, TcpClient, CERT};
 
-mod draw;
-mod simplyplural;
+mod net;
 mod wifi;
-
-extern crate alloc;
 
 #[macro_export]
 macro_rules! make_static {
@@ -48,16 +34,6 @@ fn coma(lpwr: LPWR) -> ! {
     Rtc::new(lpwr, None).sleep_deep(&[])
 }
 
-type Spi = esp_hal::spi::master::Spi<'static, SPI2, FullDuplexMode>;
-type SpiBus = RefCellDevice<'static, Spi, gpio::Output<'static, gpio::Gpio15>, Delay>;
-type EpdDisplay = epd_waveshare::epd2in13_v2::Epd2in13<
-    SpiBus,
-    gpio::Input<'static, gpio::Gpio25>,
-    gpio::Output<'static, gpio::Gpio27>,
-    gpio::Output<'static, gpio::Gpio26>,
-    Delay,
->;
-
 #[main]
 async fn main(spawner: Spawner) {
     esp_alloc::heap_allocator!(50 * 1000);
@@ -66,7 +42,6 @@ async fn main(spawner: Spawner) {
     let system = SystemControl::new(peripherals.SYSTEM);
 
     let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
 
     esp_println::logger::init_logger(log::LevelFilter::Info);
 
@@ -78,43 +53,6 @@ async fn main(spawner: Spawner) {
             [OneShotTimer::new(timer_group0.timer0.into())]
         ),
     );
-
-    // Setup the EPD display, over the SPI bus.
-    let io = gpio::Io::new(peripherals.GPIO, peripherals.IO_MUX);
-
-    let cs = gpio::Output::new(io.pins.gpio15, gpio::Level::High);
-    let busy = gpio::Input::new(io.pins.gpio25, gpio::Pull::None);
-    let rst = gpio::Output::new(io.pins.gpio26, gpio::Level::High);
-    let dc = gpio::Output::new(io.pins.gpio27, gpio::Level::Low);
-
-    let spi = make_static!(
-        RefCell<Spi>,
-        RefCell::new(
-            Spi::new(peripherals.SPI2, 8.MHz(), SpiMode::Mode0, &clocks).with_pins(
-                Some(io.pins.gpio13), // sclk
-                Some(io.pins.gpio14), // mosi
-                gpio::NO_PIN,
-                gpio::NO_PIN,
-            ),
-        )
-    );
-
-    let mut spi_bus = RefCellDevice::new(&*spi, cs, delay).unwrap();
-
-    let mut epd = EpdDisplay::new(&mut spi_bus, busy, dc, rst, &mut delay, None)
-        .expect("EPaper should be present");
-
-    let display = make_static!(EpdBuffer, EpdBuffer::default());
-    display.set_rotation(DisplayRotation::Rotate90);
-
-    let font = Font::try_from_bytes(include_bytes!("../Comfortaa-Medium-Latin.ttf")).unwrap();
-    let mut display_error = |text: &str| {
-        log::info!("{text}");
-        draw::text_to_display(display, font.clone(), text);
-
-        epd.update_and_display_frame(&mut spi_bus, display.buffer(), &mut delay)
-            .expect("EPaper should accept update/display requests");
-    };
 
     // Setup the WIFI connection.
     let wifi_stack = match wifi::connect(
@@ -129,52 +67,26 @@ async fn main(spawner: Spawner) {
     {
         Ok(stack) => stack,
         Err(err) => {
-            display_error(&format!("Failed to connect to wifi: {err:?}"));
+            log::info!("Failed to connect to wifi: {err:?}");
             coma(peripherals.LPWR);
         }
     };
 
     // Setup HTTPS client
-    let client = simplyplural::init_http_client(wifi_stack, peripherals.RSA);
 
-    spawner.must_spawn(main_loop(epd, &mut *display, spi_bus, client, font, delay));
-}
+    let state = make_static!(TcpClientState<1, 4096, 4096>, TcpClientState::new());
+    let tcp_client = make_static!(TcpClient, TcpClient::new(wifi_stack, &*state));
+    let dns_socket = make_static!(DnsSocket, DnsSocket::new(wifi_stack));
 
-#[embassy_executor::task]
-async fn main_loop(
-    mut epd: EpdDisplay,
-    display: &'static mut EpdBuffer,
-    mut spi_bus: SpiBus,
-    mut http: simplyplural::HttpClient,
-    font: Font<'static>,
-    mut delay: Delay,
-) {
-    log::info!("Starting main loop");
+    let config = reqwless::client::TlsConfig::new(
+        reqwless::TlsVersion::Tls1_3,
+        reqwless::Certificates {
+            ca_chain: Some(reqwless::X509::pem(CERT).unwrap()),
+            ..Default::default()
+        },
+        Some(make_static!(RSA, peripherals.RSA)), // Will use hardware acceleration
+    );
 
-    let mut prev_text = heapless::String::new();
-    loop {
-        let text = match simplyplural::fetch_current_front_name(&mut http).await {
-            Ok(text) => text,
-            Err(err) => {
-                log::info!("{err:?}");
-
-                let mut string = format!("Err: {err:?}");
-                string.truncate(32);
-
-                heapless::String::from_str(&string).unwrap()
-            }
-        };
-
-        if text != prev_text {
-            draw::clear_display(display);
-            draw::text_to_display(display, font.clone(), text.trim_end());
-
-            epd.update_and_display_frame(&mut spi_bus, display.buffer(), &mut delay)
-                .expect("EPaper should accept update/display requests");
-
-            prev_text = text;
-        }
-
-        Timer::after_secs(10).await;
-    }
+    let mut client = HttpClient::new_with_tls(&*tcp_client, &*dns_socket, config);
+    net::perform_request(&mut client).await.unwrap();
 }
