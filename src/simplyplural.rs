9da0c027053aa27e6873fe9ab39ca7f36bf2fb95
GnomedDev <david2005thomas@gmail.com>
2024-07-16 02:13:28 +0100

    WIP

--- src/simplyplural.rs
+++ src/simplyplural.rs
@@ -1,21 +1,65 @@
-use embedded_io::{Read, Write};
-use heapless::{String, Vec};
+use aformat::{aformat, CapStr};
+use embassy_net::{tcp::client::TcpClientState, Stack};
+use esp_hal::peripherals::RSA;
+use heapless::String;
+use reqwless::request::{Method, RequestBuilder as _};
 
-use esp_wifi::{wifi::WifiStaDevice, wifi_interface::Socket};
+use crate::make_static;
 
-use crate::SocketError;
+type WifiDriver = esp_wifi::wifi::WifiDevice<'static, esp_wifi::wifi::WifiStaDevice>;
 
-pub fn fetch_current_front_name(
-    socket: &mut Socket<'_, '_, WifiStaDevice>,
-) -> Result<String<32>, SocketError> {
-    socket.write(b" ")?;
-    socket.flush()?;
+type DnsSocket = embassy_net::dns::DnsSocket<'static, WifiDriver>;
+type TcpClient = embassy_net::tcp::client::TcpClient<'static, WifiDriver, 1, 4096, 4096>;
 
-    let mut buf = [0; 32];
-    socket.read_exact(&mut buf).map_err(|err| match err {
-        embedded_io::ReadExactError::UnexpectedEof => SocketError::SocketClosed,
-        embedded_io::ReadExactError::Other(other) => other,
-    })?;
+pub type HttpClient = reqwless::client::HttpClient<'static, TcpClient, DnsSocket>;
 
-    Ok(String::from_utf8(Vec::from_slice(&buf).unwrap()).unwrap())
+static CERT: &[u8; 3163] = concat_bytes!(include_bytes!("../apparyllis-com-chain.pem"), b'\0');
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SPResponse {
+    front_string: String<32>,
+}
+
+pub fn init_http_client(wifi_stack: &'static Stack<WifiDriver>, rsa: RSA) -> HttpClient {
+    let state = make_static!(TcpClientState<1, 4096, 4096>, TcpClientState::new());
+    let tcp_client = make_static!(TcpClient, TcpClient::new(wifi_stack, &*state));
+    let dns_socket = make_static!(DnsSocket, DnsSocket::new(wifi_stack));
+
+    let config = reqwless::client::TlsConfig::new(
+        reqwless::TlsVersion::Tls1_3,
+        reqwless::Certificates {
+            ca_chain: Some(reqwless::X509::pem(CERT).unwrap()),
+            ..Default::default()
+        },
+        Some(make_static!(RSA, rsa)), // Will use hardware acceleration
+    );
+
+    HttpClient::new_with_tls(&*tcp_client, &*dns_socket, config)
+}
+
+#[allow(clippy::large_futures)]
+pub async fn fetch_current_front_name(
+    http: &mut HttpClient,
+) -> Result<String<32>, reqwless::Error> {
+    let url = aformat!(
+        "https://api.apparyllis.com/v1/friend/{}/getFrontValue",
+        CapStr::<32>(env!("SP_ID"))
+    );
+
+    let headers = [("Authorization", env!("SP_KEY"))];
+
+    log::info!("Connecting to {url}");
+    let mut request = http.request(Method::GET, &url).await?.headers(&headers);
+
+    log::info!("Sending request");
+    let mut rx_buf = [0; 1024];
+    let resp = request.send(&mut rx_buf).await?;
+
+    log::info!("Reading body");
+    let body = resp.body().read_to_end().await?;
+
+    log::info!("Parsing response");
+    let resp_json: SPResponse = serde_json::from_slice(body).unwrap();
+    Ok(resp_json.front_string)
 }
