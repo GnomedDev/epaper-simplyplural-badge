fdc86000eb1d0480c9216a702c84f605cef51957
GnomedDev <david2005thomas@gmail.com>
2024-07-17 22:22:34 +0100

    Filter out non-alphabetic characters

--- src/simplyplural.rs
+++ src/simplyplural.rs
@@ -3,6 +3,7 @@ use core::str::FromStr;
 use aformat::{aformat, CapStr};
 use heapless::String;
 use reqwless::request::{Method, RequestBuilder as _};
+use serde::{de::Error as _, Deserialize as _};
 
 type WifiDriver = esp_wifi::wifi::WifiDevice<'static, esp_wifi::wifi::WifiStaDevice>;
 
@@ -11,10 +12,34 @@ type TcpClient<'a> = embassy_net::tcp::client::TcpClient<'a, WifiDriver, 1, 8192
 
 pub type HttpClient<'a> = reqwless::client::HttpClient<'a, TcpClient<'a>, DnsSocket<'a>>;
 
+fn filter_characters<'de, const N: usize, D: serde::Deserializer<'de>>(
+    deserializer: D,
+) -> Result<String<N>, D::Error> {
+    let str = serde_cow::CowStr::deserialize(deserializer)?.0;
+
+    let mut out = String::new();
+    for char in str.chars() {
+        if !char.is_alphabetic() && !['?', ' ', '(', ')', ','].contains(&char) {
+            continue;
+        }
+
+        if out.push(char).is_err() {
+            return Err(D::Error::invalid_length(
+                str.len(),
+                &aformat!("A string that fit into {N} bytes").as_str(),
+            ));
+        }
+    }
+
+    Ok(out)
+}
+
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct SPResponse {
+    #[serde(deserialize_with = "filter_characters")]
     front_string: String<32>,
+    #[serde(deserialize_with = "filter_characters")]
     custom_front_string: String<32>,
 }
 
